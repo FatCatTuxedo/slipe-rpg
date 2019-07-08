e122f1ce9f920637cff5eb779d963e10156df058
Jamie J <jamie@myimprovementnetwork.com>
2019-07-08 13:46:05 +0000

    added teams

---
+++ Source/ServerSide/mTeam.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Slipe.MtaDefinitions;
+using Slipe.Server.Game;
+using Slipe.Shared.Utilities;
+
+namespace ServerSide
+{
+    public static class mTeam
+    {
+        public static vTeam Staff = (vTeam)new Team("Staff", Color.White);
+        public static vTeam AuxStaff = (vTeam)new Team("Auxiliary Staff", Color.GreenYellow);
+        public static vTeam Police = (vTeam)new Team("Police", Color.LightBlue);
+        public static vTeam Civs = (vTeam)new Team("Civilians", Color.Yellow);
+        public static vTeam Criminals = (vTeam)new Team("Criminals", Color.Red);
+        public static vTeam Gangsters = (vTeam)new Team("Gangsters", Color.Purple);
+    }
+}
---
+++ Source/ServerSide/vTeam.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Slipe;
+using Slipe.Server.IO;
+using Slipe.Server.Elements;
+using Slipe.Shared.Elements;
+using Slipe.Server.Game;
+using Slipe.MtaDefinitions;
+
+namespace ServerSide
+{
+    [DefaultElementClass(ElementType.Team)]
+    public class vTeam : Team
+    {
+        public vTeam(MtaElement element) : base(element)
+        {
+
+        }
+    }
+}
